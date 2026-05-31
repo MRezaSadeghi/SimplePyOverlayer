5034d45a01c3814da0b188faa4742d6bbaea4147
MRezaSadeghi <sadeghimohammadreza77@gmail.com>
2026-05-31 07:00:07 +0330

    some inital function plus a guide added

---
+++ overlayer.py
@@ -0,0 +1,14 @@
+from config import paths
+
+
+class Overlayer:
+    def __init__(self):
+        self.text_folder = paths.RAW_TEXT_FOLDER
+        self.video_folder = paths.RAW_VIDEO_FOLDER
+        self.export_folder = paths.EXPORT_FOLDER
+
+    def read_overlay_text(self):
+        pass
+
+    def add_overlay_text(self):
+        pass
